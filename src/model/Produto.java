5b9ecb2beb0451eae09a0e0aa2b0b9894164fefe
lavezzo <lavezzo23@gmail.com>
2015-11-12 20:24:29 -0200

    10  Criei tela e janela do objeto produto.

---
+++ src/model/Produto.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer id;
+    private String descricao;
+    private Calendar dataValidade;
+    //private Fornecedor idFornecedor; necessario ligar a variavel aos fornecedores cadastrados
+    private double precoCompra;
+    private double precoVenda;
+    private double quantidadeEstoque;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Calendar getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Calendar dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public double getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(double quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+}
